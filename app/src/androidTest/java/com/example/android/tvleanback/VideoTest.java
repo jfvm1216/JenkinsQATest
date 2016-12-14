cb6285a5a9caac4c54d8ea917de757f90522773e
jvazquez <jvazquez>
2016-12-14 09:21:23 -0600

    Test 1

--- app/src/androidTest/java/com/example/android/tvleanback/VideoTest.java
+++ app/src/androidTest/java/com/example/android/tvleanback/VideoTest.java
@@ -20,7 +20,7 @@ public class VideoTest {
     public void VideoParceableTest() {
         List<Video> testVideoList = new ArrayList<>();
         testVideoList.add(new Video.VideoBuilder()
-                .title("Dragon Movie city")
+                .title("Dragon Movie ")
                 .description("A delightful kids movie")
                 .studio("Dream Movies")
                 .id((long) (Math.random()*100L))
